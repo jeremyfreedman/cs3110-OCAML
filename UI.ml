26278c07555dfb94097e386ec7a8c16f71096837
Jeremy Freedman <jmf365@cornell.edu>
2020-04-25 02:32:49 -0400

    Added list all instruction

--- UI.ml
+++ UI.ml
@@ -28,6 +28,25 @@ let print_tracks state =
     List.hd (state.library |> list_tracks |> List.sort compare |> List.map
                (fun s -> ANSITerminal.(print_string [blue] ("- " ^ s ^ "\n"))))
 
+let rec print_all state artists =
+  match artists with 
+  | [] -> ()
+  | h::t -> ANSITerminal.
+              (print_string [blue;Bold] (h.name ^ "\n");
+               List.hd (List.map
+                          (fun x -> ANSITerminal.(print_string [blue] ("\t" ^ x.title ^ "\n");
+                                                  List.hd (List.map (fun y -> ANSITerminal.(print_string [cyan] ("\t - " ^ y ^ "\n"))) x.tracks))) h.albums);
+               print_newline ());print_all state t
+
+let print_list state input = 
+  if (List.length input = 1) then
+    (ANSITerminal.(print_string [white;on_red] "Usage: list <all|artists|albums|tracks>");print_newline ();)
+  else if (List.nth input 1 = "all") then print_all state (state.library |> list_artists)
+  else if (List.nth input 1 = "artists") then print_artists state 
+  else if (List.nth input 1 = "albums") then print_albums state 
+  else if (List.nth input 1 = "tracks") then print_tracks state 
+  else (ANSITerminal.(print_string [white;on_red] "Usage: list <all|artists|albums|tracks>");print_newline ();)
+
 let print_libinfo state = 
   let artists = state.library |> list_artists |> List.length |> string_of_int in 
   let albums = state.library |> list_albums |> List.length |> string_of_int in 
@@ -35,14 +54,6 @@ let print_libinfo state =
   if (artists = "0") then
     ANSITerminal.(print_string [white;on_red] "No library loaded") else
     ANSITerminal.(print_string [blue] (artists ^ " artists\n" ^ albums ^ " albums\n" ^ tracks ^ " tracks" ));print_newline ()
-
-let print_list state input = 
-  if (List.length input = 1) then
-    (ANSITerminal.(print_string [white;on_red] "Usage: list <artists|albums|tracks|>");print_newline ();)
-  else if (List.nth input 1 = "artists") then print_artists state 
-  else if (List.nth input 1 = "albums") then print_albums state 
-  else if (List.nth input 1 = "tracks") then print_tracks state 
-  else (ANSITerminal.(print_string [white;on_red] "Usage: list <artists|albums|tracks|>");print_newline ();)
 
 let restart state = ANSITerminal.(print_string [white;on_red] "Restarting...");print_newline();set_library (load_library "blank.json") state
 
@@ -52,7 +63,7 @@ let print_help () =
                 print_endline "\tload <filename>\t\t\tLoads a library from a OCAML-compatible JSON file.";
                 print_endline "\tmklibrary <filename>\t\tCreates a new library with the given filename.";
                 print_endline "\tlibinfo\t\t\t\tGives stats about the currently open library.";
-                print_endline "\tlist <artists|albums|tracks>\tLists the provided field.";
+                print_endline "\tlist <all|artists|albums|tracks>\tLists the provided field.";
                 print_endline "\tplay \"<trackname>\"\t\tPlays track.";
                 print_endline "\trestart\t\t\t\tRestarts OCAML (unloads library).";
                 print_endline "\tquit\t\t\t\tQuit OCAML.")
