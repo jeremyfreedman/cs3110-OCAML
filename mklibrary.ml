64c419007c9bc099b04c6e43d1083c899a431b92
Jeremy Freedman <jmf365@cornell.edu>
2020-04-25 01:57:54 -0400

    Added dir -> Library functionality

---
+++ mklibrary.ml
@@ -0,0 +1,38 @@
+open Library
+open Unix
+
+exception NoDir of string 
+
+let rec read_dir_helper dh acc =
+  match Unix.readdir dh with 
+  | exception _ -> acc 
+  | "." -> read_dir_helper dh acc 
+  | ".." -> read_dir_helper dh acc 
+  | s -> read_dir_helper dh (s::acc)
+
+let read_dir dir =
+  let dh = try
+      Unix.opendir dir
+    with Unix.Unix_error _  -> raise (NoDir dir) in 
+  List.rev (read_dir_helper dh [])
+
+let read_artists dir = read_dir dir 
+
+let read_albums dir artist = read_dir (dir ^ "/" ^ artist)
+
+let read_tracks dir artist album = read_dir (dir ^ "/" ^ artist ^ "/" ^ album)
+
+let rec make_artist dir artist albums acc = 
+  match albums with 
+  | [] -> acc
+  |  h::t -> make_artist dir artist t
+               ({title = h; tracks = (read_tracks dir artist h)}::acc)
+
+
+let make_library dir = 
+  {
+    lib_name = dir;
+    artists = List.fold_left (fun acc x ->
+        {name = x;albums = (make_artist dir x (read_albums dir x) [])}::acc)
+        [] (read_artists dir)
+  }
