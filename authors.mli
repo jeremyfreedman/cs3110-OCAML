90dc5f2a038a4cc6ef4211153a1a4915001e7adc
Jeremy Freedman <jmf365@cornell.edu>
2020-04-27 13:49:58 -0400

    Added authors unit

---
+++ authors.mli
@@ -0,0 +1,8 @@
+(**
+   Metadata about this submission.
+   @author Jeremy Freedman (jmf365)
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
