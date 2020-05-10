67ec7aa8a41a0ed9e865f7234c214b7f4206f7e8
Jeremy Freedman <jmf365@cornell.edu>
2020-05-10 14:01:51 -0400

    Updated hours

--- authors.ml
+++ authors.ml
@@ -1,4 +1,6 @@
 (* TODO: set the value below.  There should be one element
    of the list per author of the submission.  Order does
    not matter. *)
-let hours_worked = [20]
+let hours_worked = [30]
+(* Note: this is honestly a massive ballpark, I have a terrible sense of time
+   passing whenever I work. Sometimes it flies by and sometimes it crawls.*)
